8c16f37fbeb777a73aed98ebd4a15cf176302008
skocur <skocur10@gmail.com>
2020-02-27 20:30:20 +0100

    Fixed code structure

--- src/main/java/com/imagecipher/icsdk/IcEncrypter.java
+++ src/main/java/com/imagecipher/icsdk/IcEncrypter.java
@@ -7,13 +7,8 @@ import java.io.IOException;
 
 public abstract class IcEncrypter implements AutoCloseable {
 
-    private String fileName;
-    private BufferedImage image;
-
-    public IcEncrypter(String fileName) throws IOException {
-        this.fileName = fileName;
-        this.image = ImageIO.read(new File(fileName));
-    }
+    protected String fileName;
+    protected BufferedImage image;
 
     public abstract void encrypt(String text);
 
@@ -21,8 +16,9 @@ public abstract class IcEncrypter implements AutoCloseable {
         return fileName;
     }
 
-    public void setFileName(String fileName) {
+    public void setSourceFile(String fileName) throws IOException {
         this.fileName = fileName;
+        this.image = ImageIO.read(new File(fileName));
     }
 
     public BufferedImage getImage() {
